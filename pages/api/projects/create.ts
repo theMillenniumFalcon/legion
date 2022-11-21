cb71b3ca3fd13c70bc156a12f43fc3b71358a205
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-11-21 23:33:38 +0530

    create project route added

---
+++ pages/api/projects/create.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '@/lib/prisma';
+
+export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method !== 'POST') {
+        return res.status(405).send("Method not allowed")
+    }
+
+    const { name } = req.body
+    const newProject = await prisma.project.create({
+        data: { name },
+    })
+
+    res.status(200).json(newProject)
+}
