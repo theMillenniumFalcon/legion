717aabe48e8b767cfe115b16d18fee903f3bcf02
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-11-21 22:52:46 +0530

    added _document.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import type { NextPage } from 'next';
+import { Flex, Heading } from '@chakra-ui/react';
+
+const Home: NextPage = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <Flex height="300px" alignItems="center" justifyContent="center">
+      <Heading as="h1" fontWeight="800">Legion</Heading>
+    </Flex>
   )
 }
+
+export default Home
--- types/env.d.ts
+++ types/env.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DATABASE_URL: string;
+      REDIS_URL: string;
     }
   }
 }
