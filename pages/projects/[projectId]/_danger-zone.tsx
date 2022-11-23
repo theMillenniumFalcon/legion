8d0e6ea03dfaf874de2b2838f6d65de0eeb0a11b
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-11-23 19:53:05 +0530

    added danger zone

---
+++ pages/projects/[projectId]/_danger-zone.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Box, Button, Flex } from '@chakra-ui/react';
+import { DeleteIcon } from '@chakra-ui/icons';
+
+import { SectionHeading } from '../../../components/SectionHeading';
+
+interface dangerzoneProps {
+    onDelete: Function
+    buttonText: string
+    children?: React.ReactNode
+    [key: string]: any
+}
+
+export const DangerZone: React.FC<dangerzoneProps> = ({
+    onDelete,
+    buttonText,
+    children,
+    ...props
+}) => {
+    const [deleting, setDeleting] = useState(false)
+
+    const deleteHandler = async () => {
+        if (deleting) return
+
+        try {
+            setDeleting(true)
+            await onDelete()
+        } finally {
+            setDeleting(false)
+        }
+    }
+
+    return (
+        <Box {...props}>
+            <Flex justifyContent="space-between">
+                <SectionHeading heading="🚨 Danger zone">
+                    {children}
+                </SectionHeading>
+                <Button
+                    colorScheme="red"
+                    color="red.500"
+                    variant="outline"
+                    rightIcon={<DeleteIcon w="3" h="3" />}
+                    onClick={deleteHandler}
+                    isLoading={deleting}
+                >
+                    {buttonText}
+                </Button>
+            </Flex>
+        </Box>
+    )
+}
