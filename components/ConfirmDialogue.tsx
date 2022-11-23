a6b1e63592f204915ccea950152f3b4f9c8f5ee0
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-11-23 10:18:04 +0530

    updated README

---
+++ components/ConfirmDialogue.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Link } from '@chakra-ui/react';
+
+interface confirmdialogueProps { }
+
+export const ConfirmDialogue: React.FC<confirmdialogueProps> = (props) => {
+    return (
+        <>hello</>
+    )
+}
