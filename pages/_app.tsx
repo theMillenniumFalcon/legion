9aa1a2e3827b92d67bd92cbb2d95ba7ecbe1697a
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-11-23 23:30:57 +0530

    updated index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, Flex, Container, ChakraProvider } from '@chakra-ui/react';
+import '@fontsource/raleway/700.css';
+import '@fontsource/raleway/800.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
 
-export default function App({ Component, pageProps }: AppProps) {
+import { Footer } from '../components/Footer';
+import { MockDeploymentBanner } from '../components/MockDeploymentBanner';
+import { theme } from '@/lib/chakra-theme';
+
+const isMock = process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT === "true"
+
+export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      {isMock && <MockDeploymentBanner />}
+      <Box bg="gray.100">
+        <Container maxWidth="container.lg">
+          <Flex
+            direction="column"
+            pt="16"
+            pb="8"
+            px="16"
+            minH={`calc(100vh - 4rem${isMock ? ' - 2rem' : ''})`}
+            shadow="base"
+            bg="white"
+            rounded="base"
+          >
+            <Component {...pageProps} />
+          </Flex>
+        </Container>
+
+        <Footer />
+      </Box>
     </ChakraProvider>
   )
 }
