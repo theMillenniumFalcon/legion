57e57f89ad24397a8f943fbaf3072f031ff6b4ad
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-11-30 11:39:04 +0530

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Box, Flex, Container, ChakraProvider } from '@chakra-ui/react';
 import '@fontsource/raleway/700.css';
 import '@fontsource/raleway/800.css';
@@ -11,27 +12,40 @@ import { theme } from '@/lib/chakra-theme';
 const isMock = process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT === "true"
 
 export default function App({ Component, pageProps }) {
-  return (
-    <ChakraProvider theme={theme}>
-      {isMock && <MockDeploymentBanner />}
-      <Box bg="gray.100">
-        <Container maxWidth="container.lg">
-          <Flex
-            direction="column"
-            pt="16"
-            pb="8"
-            px="16"
-            minH={`calc(100vh - 4rem${isMock ? ' - 2rem' : ''})`}
-            shadow="base"
-            bg="white"
-            rounded="base"
-          >
-            <Component {...pageProps} />
-          </Flex>
-        </Container>
+  const [showChild, setShowChild] = useState(false)
+  useEffect(() => {
+    setShowChild(true)
+  }, [])
 
-        <Footer />
-      </Box>
-    </ChakraProvider>
-  )
+  if (!showChild) {
+    return null
+  }
+  if (typeof window === 'undefined') {
+    return <></>
+  } else {
+    return (
+      <ChakraProvider theme={theme}>
+        {isMock && <MockDeploymentBanner />}
+        <Box bg="gray.100">
+          <Container maxWidth="container.lg">
+            <Flex
+              direction="column"
+              pt="16"
+              pb="8"
+              px="16"
+              minH={`calc(100vh - 4rem${isMock ? ' - 2rem' : ''})`}
+              shadow="base"
+              bg="white"
+              rounded="base"
+            >
+              <Component {...pageProps} />
+            </Flex>
+          </Container>
+
+          <Footer />
+        </Box>
+      </ChakraProvider>
+    )
+  }
+
 }
