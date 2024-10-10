078aa39b8195e91e7827c077de273c52cabb54fd
Nishank Priydarshi <nishankpr2002@gmail.com>
2024-10-10 12:34:44 +0530

    chnaged footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,12 +5,8 @@ export default function Footer(props) {
     <Container maxWidth="container.lg">
       <Flex mt="auto" alignItems="center" justifyContent="space-between" textAlign="center" px="12" color="gray.500" height="16" {...props} fontSize="sm">
         <Text mt="1">
-          Made by {' '}
-          <Link href="https://themillenniumfalcon.github.io" fontWeight="500" color="blue.400" isExternal>Nishank Priydarshi</Link>
-          {', '}
-          <Link href="https://github.com/gunjan1909" fontWeight="500" color="blue.400" isExternal>Gunjan A. Bhanarkar</Link>
-          {' '}& {' '}
-          <Link href="https://www.instagram.com/nxmxn_21" fontWeight="500" color="blue.400" isExternal>Naman Kumawat</Link>
+          Built with ❤️ by {' '}
+          <Link href="https://nishank.vercel.app" fontWeight="500" color="blue.400" isExternal>Nishank Priydarshi</Link>
         </Text>
       </Flex>
     </Container>
